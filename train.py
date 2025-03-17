80972f852199576c5afebb71184a82943d7df7c5
mikayahlevi <mikayahlevi@gmail.com>
2025-03-16 18:21:56 -0600

    begin

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import time
 import colorama
+import json
 
 
 from dataclasses import dataclass
@@ -10,7 +11,6 @@ from matplotlib import pyplot
 from typing import Optional
 
 from model import transformer_config
-from sample import sample
 
 
 @dataclass
@@ -24,25 +24,19 @@ class train_config:
 
     update_every: int = 1
 
-    schedule_every: int = 10
+    schedule_every: int = 20
+
+    log_loss_every: int = 20
+    # eval on the validation dataset
+    eval_every: int = 200
 
 
-    log_minor: bool = True
-    log_major: bool = True
+    print_progress_every: int = 40
 
-    log_minor_every: int = 20
-    log_major_every: int = 200
+    
+    save_checkpoint_every: int = 1000
 
-    sample_at_log_minor: bool = False
-    save_stats_to_file_at_log_minor: bool = False
-
-    sample_at_log_major: bool = False
-    plot_at_log_major: bool = True
-    save_at_log_major: bool = False
-    save_stats_to_file_at_log_major: bool = True
-
-
-    train_run_path: Optional[str] = None
+    train_log_path: Optional[str] = None
     
     
 
@@ -119,12 +113,8 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
     print(colorama.Style.RESET_ALL, end='')
 
 
-    start = time.time()
 
-    pin_device_args = {}
-    if device != 'cpu':
-        pin_device_args['pin_memory'] = True
-        pin_device_args['pin_memory_device'] = device
+    pin_device_args = {} if device == 'cpu' else {'pin_memory': True, 'pin_memory_device': device}
 
     train_dataloader = torch.utils.data.DataLoader(dataset['train'], batch_size = settings.batch_size, shuffle = True, **pin_device_args)
     val_dataloader = torch.utils.data.DataLoader(dataset['validation'], batch_size = settings.batch_size, shuffle = True, **pin_device_args)
@@ -144,20 +134,20 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
         ]
     )
 
-    
-    train_loss_history = torch.empty(settings.total_steps // settings.log_major_every, device='cpu')
-    val_loss_history = torch.empty(settings.total_steps // settings.log_major_every, device='cpu')
-    
-    log_major_loss_total = 0.0
-    log_minor_loss_total = 0.0
 
+
+    log_train_loss_sum = 0.0
+    print_train_loss_sum = 0.0
+    print_val_loss_sum = 0.0
+    n_evals_since_print = 0
     
+    start = time.time()
     for step in range(settings.total_steps):
         model.train()
 
-        inputs, labels = next(iter(train_dataloader)).values()
+        ids = next(iter(train_dataloader))['ids'].to(device)
         
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs, labels = ids[:-1], ids[1:]
 
         logits = model(inputs)
 
@@ -165,9 +155,8 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
         loss = criterion(logits.flatten(-3, -2), labels.flatten(-2, -1))
 
 
-        # log the loss
-        log_major_loss_total += loss.item()
-        log_minor_loss_total += loss.item()
+        log_train_loss_sum += loss.item()
+        print_train_loss_sum += loss.item()
 
 
         loss.backward()
@@ -179,11 +168,77 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
             optimizer.step()
             optimizer.zero_grad()
 
+
+
         if (step + 1) % settings.schedule_every == 0:
             scheduler.step()
 
+            log_msg = f'step: {step + 1}' + 'lr: ' + '{:.4f}'.format(scheduler.get_last_lr()[0])
+            with open(settings.train_log_path + '/stats/log.txt', 'a') as file: 
+                file.write(log_msg + '\n')
+        
 
-        with torch.inference_mode():
+        if (step + 1) % settings.log_loss_every == 0:
+            log_train_loss_sum = 0.0
+            train_loss_average = log_train_loss_sum / settings.log_loss_every
+
+            log_msg = f'step: {step + 1}' + 'lr: ' + str(train_loss_average)
+            with open(settings.train_log_path + '/stats/log.txt', 'a') as file: 
+                file.write(log_msg + '\n')
+
+
+        if (step + 1) % settings.log_eval_every == 0:
+            val_loss_sum = 0.0
+
+            with torch.inference_mode():
+                for _, val_ids in iter(val_dataloader):
+                    val_ids = val_ids.to(device)
+                    val_inputs, val_labels = val_ids[:-1], val_ids[1:]
+
+                    val_logits = model(val_inputs)
+
+                    val_loss = criterion(logits.flatten(-3, -2), labels.flatten(-2, -1))
+
+                    val_loss_sum += val_loss
+            
+            val_loss_avg = val_loss_sum / len(val_dataloader)
+            
+            print_val_loss_sum += val_loss_avg
+            n_evals_since_print += 1
+
+            log_msg = f'step: {step + 1}' + 'lr: ' + str(val_loss_avg)
+            with open(settings.train_log_path + '/stats/log.txt', 'a') as file: 
+                file.write(log_msg + '\n')
+
+        
+        if (step + 1) % settings.print_progress_every == 0:
+            print_info = {
+                'time elapsed': time.strftime('%H:%M:%S', time.gmtime(time.time() - start)),
+                'done': '{:.2f}'.format(100 * (step + 1) / settings.total_steps) + '%',
+                'steps': step + 1,
+                'epochs': (step + 1) // len(train_dataloader),
+            }
+
+
+            print_info['train loss'] = print_train_loss_sum / settings.print_progress_every
+            print_train_loss_sum = 0
+
+            print_info['val loss'] = print_val_loss_sum / n_evals_since_print
+            print_val_loss_sum = 0
+
+            print_info['lr'] = '{:.4f}'.format(scheduler.get_last_lr()[0])
+
+            # print the info
+            print(colorama.Fore.YELLOW, end='')
+            print(format_info(print_info))
+            print(colorama.Style.RESET_ALL, end='')
+        
+        
+
+        if (step + 1) % settings.save_checkpoint_every == 0:
+            torch.save(model.state_dict(), settings.train_run_path + '/models/' + 'checkpoint-'  + 'step-' + str(step + 1) + '.pt')
+
+        
             model.eval()
 
             # log information
@@ -209,27 +264,6 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
                 print(colorama.Style.RESET_ALL, end='')
 
 
-                # print a sample from the model
-                if settings.sample_at_log_minor:
-                    print()
-                    print(colorama.Fore.GREEN, end='')
-                    print('sample:')
-                    print(colorama.Style.RESET_ALL, end='')
-                    sequence_start = None
-                    if hasattr(tokenizer, 'character_level'):
-                        sequence_start = 'A'
-                    else:
-                        sequence_start = '<bos>'
-                    print(sample(model, tokenizer, sequence_start = sequence_start, temperature = 1.0, max_length = settings.sequence_length, device = device))
-                    print()
-                
-
-                # save the info to a file
-                if settings.save_stats_to_file_at_log_minor:
-                    with open(settings.train_run_path + '/stats/minor-log.txt', 'a') as f:
-                        f.write(format_info(info) + '\n')
-
-
 
             if settings.log_major and (step + 1) % settings.log_major_every == 0:
                 # compute the average loss
@@ -250,79 +284,3 @@ def train(settings, hyperparameters, model, dataset, tokenizer, device):
 
                     val_loss_total += loss.item()
                 avg_val_loss = val_loss_total / len(val_dataloader)
-
-
-                # set the info to be displayed
-                info = {
-                    'time elapsed': time.strftime('%M:%S', time.gmtime(time.time() - start)),
-                    'steps': str(step + 1),
-                    'done': '{:.2f}'.format(100 * (step + 1) / settings.total_steps) + '%',
-                    'avg train loss': '{:.4f}'.format(avg_train_loss),
-                    'avg val loss': '{:.4f}'.format(avg_val_loss),
-                    'epochs': str((step + 1) // len(train_dataloader)),
-                    'lr': '{:.4f}'.format(scheduler.get_last_lr()[0])
-                }
-
-
-                # print the info
-                print(colorama.Fore.YELLOW, end='')
-                print(format_info(info))
-                print(colorama.Style.RESET_ALL, end='')
-
-
-                # set the loss history
-                train_loss_history[step // settings.log_major_every] = avg_train_loss
-                val_loss_history[step // settings.log_major_every] = avg_val_loss
-
-                # save the model
-                if settings.save_at_log_major:
-                    torch.save(model.state_dict(), settings.train_run_path + '/models/' + 'step-' + str(step + 1) + '.pt')
-
-
-                # plot the loss
-                if settings.plot_at_log_major and step > settings.log_major_every:
-                    x_axis_steps = np.arange(settings.log_major_every, step + 1 + settings.log_major_every, settings.log_major_every)
-
-                    pyplot.plot(x_axis_steps, train_loss_history[:((step + 1) // settings.log_major_every)].numpy(), label='train', color='blue')
-                    pyplot.plot(x_axis_steps, val_loss_history[:((step + 1) // settings.log_major_every)].numpy(), label='val', color='red')
-
-                    # set the plot scale to log
-                    # pyplot.yscale('log')
-                    
-                    pyplot.title('Loss vs. Step')
-                    pyplot.xlabel('Step')
-                    pyplot.ylabel('Loss')
-
-                    # set the legend
-                    pyplot.legend()
-                    
-                    # save the plot
-                    pyplot.savefig(settings.train_run_path + '/stats/loss plot.png')
-
-                    pyplot.clf()
-
-                model.train()
-
-                
-
-                # print a sample from the model
-                if settings.sample_at_log_major:
-                    print()
-                    print(colorama.Fore.GREEN, end='')
-                    print('sample:')
-                    print(colorama.Style.RESET_ALL, end='')
-                    sequence_start = None
-                    if hasattr(tokenizer, 'character_level'):
-                        sequence_start = 'A'
-                    else:
-                        sequence_start = '<bos>'
-                    print(sample(model, tokenizer, sequence_start = sequence_start, temperature = 1.0, max_length = settings.sequence_length, device = device))
-                    print()
-
-                # save the info to a file
-                if settings.save_stats_to_file_at_log_major:
-                    with open(settings.train_run_path + '/stats/major-log.txt', 'a') as f:
-                        f.write(format_info(info) + '\n')
-
-
-
